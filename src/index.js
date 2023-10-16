f68fbc7aea7d79b9687c89e44b8f63fca6790cbc
DasIstMars <hledger@bk.ru>
2023-10-16 21:01:18 +0300

    5th commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import './styles/main.scss';
 
 let mobMenuButton = document.querySelector('.mob-menu__button');
 let sideMenuOpener = document.querySelector('.wrapper--closed');
+let fogSpace = document.querySelector('.fog--closed');
 
 mobMenuButton.onclick = function() {
     sideMenuOpener.classList.add('wrapper--opened');
     sideMenuOpener.classList.remove('wrapper--closed');
+    fogSpace.classList.toggle('fog--closed')
 }
 
 let sideMobMenuButton = document.querySelector('.side-mob-menu__button');
@@ -14,4 +16,11 @@ let sideMobMenuButton = document.querySelector('.side-mob-menu__button');
 sideMobMenuButton.onclick = function() {
     sideMenuOpener.classList.remove('wrapper--opened');
     sideMenuOpener.classList.add('wrapper--closed');
+    fogSpace.classList.toggle('fog--closed')
+}
+
+fogSpace.onclick = function() {
+    sideMenuOpener.classList.remove('wrapper--opened');
+    sideMenuOpener.classList.add('wrapper--closed');
+    fogSpace.classList.toggle('fog--closed')
 }
