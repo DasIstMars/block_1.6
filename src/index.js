133edc208fe5021bcfe1e91d47189597e3a6e2db
DasIstMars <hledger@bk.ru>
2023-10-16 17:49:52 +0300

    4th commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import 'normalize.css'
 import './styles/main.scss';
+
+let mobMenuButton = document.querySelector('.mob-menu__button');
+let sideMenuOpener = document.querySelector('.wrapper--closed');
+
+mobMenuButton.onclick = function() {
+    sideMenuOpener.classList.add('wrapper--opened');
+    sideMenuOpener.classList.remove('wrapper--closed');
+}
+
+let sideMobMenuButton = document.querySelector('.side-mob-menu__button');
+
+sideMobMenuButton.onclick = function() {
+    sideMenuOpener.classList.remove('wrapper--opened');
+    sideMenuOpener.classList.add('wrapper--closed');
+}
