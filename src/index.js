71b5a4ca1866338a3d6585ba6c8b4b5ee1222495
DasIstMars <hledger@bk.ru>
2023-10-17 16:38:17 +0300

    office commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import 'normalize.css'
 import './styles/main.scss';
 
+
 let mobMenuButton = document.querySelector('.mob-menu__button');
+let sideMenuWrapper = document.querySelector('.wrapper--side');
 let sideMenuOpener = document.querySelector('.wrapper--closed');
 let fogSpace = document.querySelector('.fog--closed');
 
@@ -24,3 +26,17 @@ fogSpace.onclick = function() {
     sideMenuOpener.classList.add('wrapper--closed');
     fogSpace.classList.toggle('fog--closed')
 }
+
+
+let desktopBreakpoint = window.matchMedia( '(min-width:1366px)' );
+const breakpointChecker = function() {
+    if ( desktopBreakpoint.matches === true ) {
+        sideMenuWrapper.classList.remove('wrapper--closed');
+        sideMenuWrapper.classList.add('wrapper--open');
+    } else {
+        sideMenuWrapper.classList.add('wrapper--closed');
+        sideMenuWrapper.classList.remove('wrapper--open');  
+    }
+}
+desktopBreakpoint.addListener(breakpointChecker);
+breakpointChecker();
