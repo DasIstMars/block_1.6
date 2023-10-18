fe618989c39e650d655d1ef98037981766d30035
DasIstMars <hledger@bk.ru>
2023-10-19 00:44:59 +0300

    b4 swiper

--- src/index.js
+++ src/index.js
@@ -1,42 +1,53 @@
-import 'normalize.css'
-import './styles/main.scss';
+import "normalize.css";
+import "./styles/main.scss";
+
+let openMobMenuButton = document.querySelector(".mob-menu__button");
+let closeMobMenuButton = document.querySelector(".side-mob-menu__button");
+let sideMenuWrapper = document.querySelector(".wrapper--side");
+
+let callWraper = document.querySelector('.wrapper-call');
+let feedbackWraper = document.querySelector('.wrapper-feedback');
+let fogRightSide = document.querySelector(".fog--right-side");
+let fogLeftSide = document.querySelectorAll(".fog--left-side");
 
 
-let mobMenuButton = document.querySelector('.mob-menu__button');
-let sideMenuWrapper = document.querySelector('.wrapper--side');
-let sideMenuOpener = document.querySelector('.wrapper--closed');
-let fogSpace = document.querySelector('.fog--closed');
+let phoneButton = document.querySelectorAll(".button-phone");
+let chatButton = document.querySelectorAll(".button-chat");
+let closeButton = document.querySelectorAll(".button-close"); 
 
-mobMenuButton.onclick = function() {
-    sideMenuOpener.classList.add('wrapper--opened');
-    sideMenuOpener.classList.remove('wrapper--closed');
-    fogSpace.classList.toggle('fog--closed')
+for (let i = 0; i < phoneButton.length; i++) {
+  phoneButton[i].onclick = function () {
+    callWraper.classList.replace("wrapper--closed", "wrapper--opened");
+  }
+}
+for (let i = 0; i < chatButton.length; i++) {
+  chatButton[i].onclick = function () {
+    feedbackWraper.classList.replace("wrapper--closed", "wrapper--opened");
+  }
 }
 
-let sideMobMenuButton = document.querySelector('.side-mob-menu__button');
+openMobMenuButton.onclick = function () {
+  sideMenuWrapper.classList.replace("wrapper--closed", "wrapper--opened");
+};
 
-sideMobMenuButton.onclick = function() {
-    sideMenuOpener.classList.remove('wrapper--opened');
-    sideMenuOpener.classList.add('wrapper--closed');
-    fogSpace.classList.toggle('fog--closed')
+closeMobMenuButton.onclick = function () {
+  sideMenuWrapper.classList.replace("wrapper--opened" , "wrapper--closed");
+};
+
+fogRightSide.onclick = function () {
+  sideMenuWrapper.classList.replace("wrapper--opened" , "wrapper--closed");
+};
+
+
+for (let i = 0; i < fogLeftSide.length; i++) {
+  fogLeftSide[i].onclick = function () {
+    callWraper.classList.replace("wrapper--opened" , "wrapper--closed");
+    feedbackWraper.classList.replace("wrapper--opened" , "wrapper--closed");
+  };
 }
-
-fogSpace.onclick = function() {
-    sideMenuOpener.classList.remove('wrapper--opened');
-    sideMenuOpener.classList.add('wrapper--closed');
-    fogSpace.classList.toggle('fog--closed')
+for (let i = 0; i < closeButton.length; i++) {
+  closeButton[i].onclick = function () {
+    callWraper.classList.replace("wrapper--opened" , "wrapper--closed");
+    feedbackWraper.classList.replace("wrapper--opened" , "wrapper--closed");
+  };
 }
-
-
-let desktopBreakpoint = window.matchMedia( '(min-width:1366px)' );
-const breakpointChecker = function() {
-    if ( desktopBreakpoint.matches === true ) {
-        sideMenuWrapper.classList.remove('wrapper--closed');
-        sideMenuWrapper.classList.add('wrapper--open');
-    } else {
-        sideMenuWrapper.classList.add('wrapper--closed');
-        sideMenuWrapper.classList.remove('wrapper--open');  
-    }
-}
-desktopBreakpoint.addListener(breakpointChecker);
-breakpointChecker();
