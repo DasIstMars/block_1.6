58f6c6b5fef68bc087cb96d5107a62de10230d40
DasIstMars <hledger@bk.ru>
2023-10-19 22:27:05 +0300

    half swiper

--- src/index.js
+++ src/index.js
@@ -51,3 +51,33 @@ for (let i = 0; i < closeButton.length; i++) {
     feedbackWraper.classList.replace("wrapper--opened" , "wrapper--closed");
   };
 }
+
+/*!!!!! SWIPER !!!!!*/
+let mySwiper;
+const breakpoint = window.matchMedia( '(min-width:768px)' );
+const breakpointChecker = function() {
+  if ( breakpoint.matches === true ) {
+     if ( mySwiper !== undefined ) mySwiper.destroy( true, true );
+     return;
+  } else if ( breakpoint.matches === false ) {
+     return enableSwiper();
+  }
+};
+const enableSwiper = function() {
+  mySwiper = new Swiper('.swiper', {
+      loop: true,
+      slidesPerView: "auto",
+      spaceBetween: 16,
+      speed: 500,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      autoplay: {
+          delay: 2000,
+          disableOnInteraction: true,
+      },
+    });
+};
+breakpoint.addListener(breakpointChecker);
+breakpointChecker();
